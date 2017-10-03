fb00bc5bc7f8feb7af4977595385dd50b2230a20
Marvelousmishig <dmishig@gmail.com>
2017-10-03 11:09:02 -0600

    feat: add initial Cilk

--- patterns/map/cilk.c
+++ patterns/map/cilk.c
@@ -17,11 +17,9 @@
 // clock measurements
 #include <time.h>
 
-// openmp for parallelism
-#include <omp.h>
-
-// math utilities
 #include <math.h>
+
+#include <cilk/cilk.h>
 
 // valid pin characters
 const char* chars="0123456789";
@@ -60,60 +58,36 @@ void genpass(long passnum, char* passbuff) {
     }
 }
 
-// run parallel process for checking pins
-void start_parallel_check(char* given_key){
-  char passmatch[9]; // buffer for the matched password
-
-  // variables for the timer
-  struct timespec start_time;
-  struct timespec end_time;
-  long msec;
-
-  clock_gettime(CLOCK_MONOTONIC,&start_time);
-
-  long max_comb = pow(10,8);
-  int notfound=1;
-  int answer;
-  int can_proceed = 1;
-  omp_lock_t lockA;
-  omp_init_lock(&lockA);
-
-  #pragma omp parallel for
-  for(int i=0; i<max_comb; i++){
-    if(can_proceed == 1){
-      // prevents from starting other parallel processes once answer is found
-      // this process is achieveing at least 2X speedup
-      genpass(i,passmatch);
-      notfound=test(given_key, passmatch);
-      if(!notfound){
-        // set can_proceed and answer
-        omp_set_lock(&lockA);
-        can_proceed = 0;
-        answer = i;
-        omp_unset_lock(&lockA);
-      }
-    }
-  }
-  clock_gettime(CLOCK_MONOTONIC,&end_time);
-
-  // convert the time to elapsed milliseconds
-  msec = (end_time.tv_sec - start_time.tv_sec)*1000 + (end_time.tv_nsec - start_time.tv_nsec)/1000000;
-
-  // handling non-determinism caused by parallelism
-  if(can_proceed == 1){
-    printf("Answer did not come out. Running the process again ...\n");
-    start_parallel_check(given_key);
-  }else{
-    printf("found: %8d in %dms\n",answer,msec);
-  }
-}
-
 int main(int argc, char** argv) {
     if(argc != 2) {
         printf("Usage: %s <password hash>\n",argv[0]);
         return 1;
     }
-    start_parallel_check(argv[1]);
 
+    char passmatch[9]; // buffer for the matched password
+    long currpass=0; // current password under consideration
+
+    // variables for the timer
+    struct timespec start_time;
+    struct timespec end_time;
+    long msec;
+
+    clock_gettime(CLOCK_MONOTONIC,&start_time);
+
+    // While a match has not been found, search
+    int notfound=1;
+    long max_comb = pow(10,8);
+
+    cilk_for (int i = 0; i < max_comb; ++i){
+      genpass(i,passmatch); // generate the password
+      notfound=test(argv[1], passmatch); // check for a match
+      if(!notfound) printf("found: %d\n",i);
+    }
+    clock_gettime(CLOCK_MONOTONIC,&end_time);
+
+    // convert the time to elapsed milliseconds
+    msec = (end_time.tv_sec - start_time.tv_sec)*1000 + (end_time.tv_nsec - start_time.tv_nsec)/1000000;
+
+    printf("found: %s in %dms\n",passmatch,msec);
     return EXIT_SUCCESS;
 }
