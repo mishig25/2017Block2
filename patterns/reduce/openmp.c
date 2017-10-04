7b941cda74e4d2dfc9f0868c534de1b3c1d454e9
Marvelousmishig <dmishig@gmail.com>
2017-10-04 10:39:11 -0600

    chore: refactor funcs

--- patterns/reduce/openmp.c
+++ patterns/reduce/openmp.c
@@ -42,41 +42,34 @@ void volume_append(struct volume* v, struct phaseball* o, int ctr) {
 }
 
 // Place phaseballs uniformly in a box, with mass equal to the manhattan distance
-void place_uniformly(int sx, int ex, int sy, int ey, int sz, int ez, struct volume* v, struct SoA* soa) {
+int place_uniformly(int sx, int ex, int sy, int ey, int sz, int ez, struct SoA* soa){
   int counter = 0;
   int number_of_phaseballs = (ex-sx+1)*(ey-sy+1)*(ez-sy+1);
   soa->x = (float *) malloc(number_of_phaseballs * sizeof(float));
   soa->y = (float *) malloc(number_of_phaseballs * sizeof(float));
   soa->z = (float *) malloc(number_of_phaseballs * sizeof(float));
   soa->mass = (float *) malloc(number_of_phaseballs * sizeof(float));
-    for(int i=sx; i<=ex; i++) {
+  for(int i=sx; i<=ex; i++) {
         for(int j=sy; j<=ey; j++) {
             for(int k=sz; k<=ez; k++) {
-                struct phaseball* n = malloc(sizeof(struct phaseball));
-                n->x = i;
-                n->y = j;
-                n->z = k;
-                n->mass = 1;
-                n->mass = fabs(n->x)+fabs(n->y)+fabs(n->z);
                 soa->x[counter] = i;
                 soa->y[counter] = j;
                 soa->z[counter] = k;
                 soa->mass[counter] = fabs(i)+fabs(j)+fabs(k);
-                volume_append(v,n,counter);
                 counter ++;
             }
         }
     }
+  return number_of_phaseballs;
 }
 
-// Projects 3D volume to 11x11 2D map and report centroid
-void post_process(struct volume* v, float* cx, float* cy, struct SoA* soa) {
+void post_process(struct SoA* soa, float* cx, float* cy, int length) {
     double mass_sum=0.0;
     double wx=0.0;
     double wy=0.0;
 
     #pragma omp parallel for reduction(+:mass_sum,wx,wy)
-    for(int i=0; i<v->last; i++){
+    for(int i=0; i<length; i++){
       // struct phaseball* o = v->objects[i];
       mass_sum += soa->mass[i];
       wx += soa->x[i] * soa->mass[i];
@@ -98,16 +91,16 @@ int main(int argc, char** argv) {
 
     // struct of arrays
     struct SoA soa;
-
     // Set the initial configuration
-    place_uniformly(-1000,1000,-100,100,-100,100,&v,&soa);
+    // place_uniformly(-1000,1000,-100,100,-100,100,&v,&soa);
 
+    int length_of_arr = place_uniformly(-1000,1000,-100,100,-100,100,&soa);
     // Generate output
     float cx,cy;
     struct timespec start_time;
     struct timespec end_time;
     clock_gettime(CLOCK_MONOTONIC,&start_time);
-    post_process(&v, &cx, &cy, &soa);
+    post_process(&soa, &cx, &cy, length_of_arr);
     clock_gettime(CLOCK_MONOTONIC,&end_time);
     long msec = (end_time.tv_sec - start_time.tv_sec)*1000 + (end_time.tv_nsec - start_time.tv_nsec)/1000000;
 
