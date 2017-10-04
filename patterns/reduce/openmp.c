4b36220cab22d85bd0cf1101a7b92a77a372b3c0
Marvelousmishig <dmishig@gmail.com>
2017-10-04 10:45:30 -0600

    chore: clean SoA

--- patterns/reduce/openmp.c
+++ patterns/reduce/openmp.c
@@ -18,12 +18,16 @@ struct SoA{
 
 // Place phaseballs uniformly in a box, with mass equal to the manhattan distance
 int place_uniformly(int sx, int ex, int sy, int ey, int sz, int ez, struct SoA* soa){
+  // current phaseball
   int counter = 0;
+  // number of phaseballs
   int number_of_phaseballs = (ex-sx+1)*(ey-sy+1)*(ez-sy+1);
+  // allocate memeory for the arrays
   soa->x = (float *) malloc(number_of_phaseballs * sizeof(float));
   soa->y = (float *) malloc(number_of_phaseballs * sizeof(float));
   soa->z = (float *) malloc(number_of_phaseballs * sizeof(float));
   soa->mass = (float *) malloc(number_of_phaseballs * sizeof(float));
+  
   for(int i=sx; i<=ex; i++) {
         for(int j=sy; j<=ey; j++) {
             for(int k=sz; k<=ez; k++) {
@@ -60,9 +64,7 @@ void post_process(struct SoA* soa, float* cx, float* cy, int length) {
 int main(int argc, char** argv) {
     // struct of arrays to store attributes
     struct SoA soa;
-    // Set the initial configuration
-    // place_uniformly(-1000,1000,-100,100,-100,100,&v,&soa);
-
+    // Set the initial configuration and get the number of phaseballs
     int length_of_arr = place_uniformly(-1000,1000,-100,100,-100,100,&soa);
     // Generate output
     float cx,cy;
