6aa1ac5e916ccc59367719fa53962e580e8d55a4
Marvelousmishig <dmishig@gmail.com>
2017-10-03 20:43:52 -0600

    feat: openmp parallel post_process func

--- patterns/reduce/compile.sh
+++ patterns/reduce/compile.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
 # Compile the serial version
-gcc -o centroid_serial serial.c 
+gcc -o serial serial.c
 
 # Compile the OpenMP version
-gcc -o centroid_openmp -fopenmp openmp.c
+gcc -o openmp -fopenmp openmp.c
 
-# Compile the cilkplus version
-gcc -o centroid_cilk -fcilkplus cilk.c
-
-# Compile the tbb version
-g++ -o centroid_tbb tbb.c -ltbb
-
-# Compile the MPI version
-mpicc -o centroid_mpi mpi.c
+# # Compile the cilkplus version
+# gcc -o centroid_cilk -fcilkplus cilk.c
+#
+# # Compile the tbb version
+# g++ -o centroid_tbb tbb.c -ltbb
+#
+# # Compile the MPI version
+# mpicc -o centroid_mpi mpi.c
