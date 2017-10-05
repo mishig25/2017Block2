b4bcfebd9e742414568dc8877bf842c038abc5ab
Marvelousmishig <dmishig@gmail.com>
2017-10-04 19:32:18 -0600

    refactor OpenMp

--- patterns/stencil/compile2.sh
+++ patterns/stencil/compile2.sh
@@ -1,4 +1,4 @@
 srun g++ -o stencil_serial serial.cpp -lopencv_core -lopencv_imgcodecs
-# srun g++ -o stencil_openmp -fopenmp openmp.cpp -lopencv_core -lopencv_imgcodecs
+srun g++ -o openmp -fopenmp openmp.cpp -lopencv_core -lopencv_imgcodecs
 # srun g++ -o stencil_tbb tbb.cpp -ltbb -lopencv_core -lopencv_imgcodecs
 # srun mpic++ -o stencil_mpi mpi.cpp -lopencv_core -lopencv_imgcodecs
