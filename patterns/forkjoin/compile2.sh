a8b12c025882a8851dfd2149bf6f12233c4a89b3
Marvelousmishig <dmishig@gmail.com>
2017-10-05 09:48:36 -0600

    feat: pull forkjoin example

---
+++ patterns/forkjoin/compile2.sh
@@ -0,0 +1,4 @@
+srun g++ -o starhole_serial serial.cpp -lopencv_core -lopencv_imgcodecs  
+srun g++ -o starhole_openmp -fopenmp openmp.cpp -lopencv_core -lopencv_imgcodecs 
+srun g++ -o starhole_tbb tbb.cpp -ltbb -lopencv_core -lopencv_imgcodecs
+srun mpic++ -o starhole_mpi mpi.cpp -lopencv_core -lopencv_imgcodecs
