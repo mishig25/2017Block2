9ee63d05c95d89a491d189df6ab00941c665ac51
Marvelousmishig <dmishig@gmail.com>
2017-10-06 11:09:15 -0600

    feat: cilk forkjoin

--- patterns/forkjoin/compile.sh
+++ patterns/forkjoin/compile.sh
@@ -5,9 +5,6 @@ gcc -g -o serial serial.c
 
 # # Compile the OpenMP version
 # gcc -o openmp -fopenmp openmp.c
-# 
-# # Compile the cilkplus version
-# g++ -o cilk -fcilkplus cilk.c
 #
-# # Compile the tbb version
-# g++ -o tbb tbb.c -ltbb
+# Compile the cilkplus version
+g++ -o cilk -fcilkplus cilk.c
