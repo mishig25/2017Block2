fa257cd596b0e5b70593130d78a84f60a94f4226
Marvelousmishig <dmishig@gmail.com>
2017-10-06 13:29:21 -0600

    chore: make OpenMP hash-based

--- patterns/forkjoin/openmp.cpp
+++ patterns/forkjoin/openmp.cpp
@@ -7,6 +7,7 @@
 #include <math.h>
 #include <time.h>
 #include <omp.h>
+#include <map>
 
 long q(long n) {
     if(n<3) return 1;
@@ -19,6 +20,26 @@ long q(long n) {
     return x + y;
 }
 
+/* uses hashmap to improve runtime */
+long q(long n, std::map<int, int> *q_vals) {
+    if (q_vals->find(n) == q_vals->end()) {
+      int val = 1;
+      if(!(n<3)){
+        // fork
+        int x,y;
+        #pragma omp task shared(x)
+        x = q(n - q(n-1,q_vals),q_vals);
+        #pragma omp task shared(y)
+        y = q(n-q(n-2,q_vals),q_vals);
+        // join
+        #pragma omp taskwait
+        val = x + y;
+      }
+      (*q_vals)[n] = val;
+    }
+    return q_vals->at(n);
+}
+
 int main(int argc, char** argv) {
     long out;
     if(argc!=2) {
@@ -26,11 +47,12 @@ int main(int argc, char** argv) {
         return 1;
     }
     long n = strtol(argv[1],NULL,10);
+    std::map<int, int> q_vals; // defining hashmap that will store q vals
 
     struct timespec start_time;
     struct timespec end_time;
     clock_gettime(CLOCK_MONOTONIC,&start_time);
-    out = q(n);
+    out = q(n, &q_vals);
     clock_gettime(CLOCK_MONOTONIC,&end_time);
     long msec = (end_time.tv_sec - start_time.tv_sec)*1000 + (end_time.tv_nsec - start_time.tv_nsec)/1000000;
 
