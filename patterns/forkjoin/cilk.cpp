2e293c77f02b456a6ae3f4ae3795add690a1a37c
Marvelousmishig <dmishig@gmail.com>
2017-10-06 13:25:02 -0600

    chore: add hashmap to q func

--- patterns/forkjoin/cilk.cpp
+++ patterns/forkjoin/cilk.cpp
@@ -9,20 +9,21 @@
 #include <cilk/cilk.h>
 #include <map>
 
-std::map<int, int> q_vals;
-
-long q(long n) {
-    if ( q_vals.find(n) == q_vals.end() ) {
+/* uses hashmap to improve runtime */
+long q(long n, std::map<int, int> *q_vals) {
+    if ((*q_vals).find(n) == (*q_vals).end()) {
       int val = 1;
       if(!(n<3)){
-        int x = cilk_spawn q(n - q(n-1));
-        int y = q(n-q(n-2));
+        // fork
+        int x = cilk_spawn q(n - q(n-1,q_vals),q_vals);
+        int y = q(n-q(n-2,q_vals),q_vals);
+        // join
         cilk_sync;
         val = x + y;
       }
-      q_vals[n] = val;
+      (*q_vals)[n] = val;
     }
-    return q_vals.at(n);
+    return (*q_vals).at(n);
 }
 
 int main(int argc, char** argv) {
@@ -32,11 +33,12 @@ int main(int argc, char** argv) {
         return 1;
     }
     long n = strtol(argv[1],NULL,10);
+    std::map<int, int> q_vals; // defining hashmap that will store q vals
 
     struct timespec start_time;
     struct timespec end_time;
     clock_gettime(CLOCK_MONOTONIC,&start_time);
-    out = q(n);
+    out = q(n, &q_vals);
     clock_gettime(CLOCK_MONOTONIC,&end_time);
     long msec = (end_time.tv_sec - start_time.tv_sec)*1000 + (end_time.tv_nsec - start_time.tv_nsec)/1000000;
 
